6644ce5ab855f53857b99184b19f0ca4dd479bcc
Julia Matos <juliaribeiro@outlook.com>
2020-10-31 21:25:32 -0300

    Changed css properties

--- src/App.js
+++ src/App.js
@@ -34,6 +34,9 @@ class App extends React.Component {
           <Cards data={data} />
           <CountryPicker handleCountryChange={this.handleCountryChange}/>
           <Chart data={data} country={country} />
+          <footer>
+          <div >Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+          </footer>
         </div>
     );
   }
